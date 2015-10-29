bf7cb3c16d27c14c019e515788361d57e213075c
Hazem AbuMostafa <hbluebrince@gmail.com>
2015-10-29 18:03:09 +0200

    Testing: Supporting spacebar and enter clicks

--- ripple.js
+++ ripple.js
@@ -3,23 +3,23 @@
 const animationTick = 20;
 const defaultOpacity = '0.60';
 var tempPressedButton = null; //Used in global mouseUp listener.
+var initialSize = 18;
 
 
 
 /**** LISTENERS ***/
 
-/*** mouseDown for every ripple element **/
-var downListen = function(e){
-	var container = this.getElementsByClassName('circles')[0];
+/*** mouseDown or keyDown for every ripple element **/
+function down(elem, e, x, y){
+	var container = elem.getElementsByClassName('circles')[0];
 	
-	var initialSize = 18;
-	var transX = e.pageX - this.offsetLeft - (initialSize/2);
-	var transY = e.pageY - this.offsetTop  - (initialSize/2);
+	var transX = x;
+	var transY = y;
 
 	var circle = document.createElement('div');
 	circle.className = 'circle';
-	circle.style.background = this.dataset.rippleColor || 'white';
-	circle.style.opacity = this.dataset.rippleOpacity || defaultOpacity;
+	circle.style.background = elem.dataset.rippleColor || 'white';
+	circle.style.opacity = elem.dataset.rippleOpacity || defaultOpacity;
 	circle.style.height = initialSize+'px';
 	circle.style.width = initialSize+'px';
 
@@ -27,16 +27,37 @@ var downListen = function(e){
 	circle.style.top  = transY+'px';
 
 	container.insertBefore(circle, container.firstChild); //Insert top.
-	tempPressedButton = this;
+	tempPressedButton = elem;
 
 	/*** Scale Animation **/
 	circle.scale = 1;
 	requestAnimationFrame(function(){ scale(circle,0.4,7) }, animationTick);
 }
 
+function mouseDownListen(e){
+	var x = e.pageX - this.offsetLeft - (initialSize/2),
+		y = e.pageY - this.offsetTop  - (initialSize/2)
+	
+	down(this, e, x, y);
+}
+
+function keyDownListen(e){
+	if(e.which != 32 && e.which != 13)
+		return;
+	else if(this.active)
+		return;
+	
+	var x = (this.clientWidth/2)  - (initialSize/2),
+		y = (this.clientHeight/2) - (initialSize/2) * 2;
+	
+	down(this, e, x, y);
+	
+	this.active = true;
+}
+
 
 /*** mouseUp global document listener for all ripple elements **/
-var upListen = function(e){
+function upListen(e){
 	if(tempPressedButton == null) return;
 	var circle = tempPressedButton.getElementsByClassName('circles')[0].firstChild;
 
@@ -48,6 +69,8 @@ var upListen = function(e){
 		requestAnimationFrame(function(){ scale(circle,0.05,8) }, animationTick);
 		requestAnimationFrame(function(){ fadeOut(circle,opacityStep) }, animationTick);
 	}
+	
+	this.active = false;
 }
 
 
@@ -86,7 +109,13 @@ function addRipples(item){
 	container.className = 'circles';
 	item.insertBefore(container, item.firstChild);
 
-	item.addEventListener('mousedown', downListen);
+	item.addEventListener('mousedown', mouseDownListen);
+	item.addEventListener('keydown', keyDownListen);
+	item.addEventListener('blur', function(e){
+		if(e.target !== tempPressedButton)
+			return;
+		upListen(e);
+	});
 }
 
 
@@ -94,6 +123,12 @@ function addRipples(item){
 
 //Assigning global up listener:
 document.addEventListener('mouseup', upListen);
+document.addEventListener('keyup', function(e){
+	if(e.which != 32 && e.which != 13)
+		return;
+	upListen(e);
+	e.target.active = false;
+});
 
 //Assigning down listener for each ripple elements:
 var items = document.getElementsByClassName('ripple');
