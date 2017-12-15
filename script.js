beb20250835a6402ce18b4482de41d2e223bbb8b
Hazem AbuMostafa <hbluebrince@gmail.com>
2017-12-15 08:38:21 +0200

    Adding title, version, and date to header (2)

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
- /*
- * Ripples
- * -------
- * 
- * Version 2.0, 2017-12-15
- *
- * (c) Copyright 2017 Hazem AbuMostafa.
- * This project is subject to the Apache License, Version 2.0
- * <http://apache.org/licenses/LICENSE-2.0.html>.
- */
+/*
+* Ripples
+* -------
+* 
+* Version 2.0, 2017-12-15
+*
+* (c) Copyright 2017 Hazem AbuMostafa.
+* This project is subject to the Apache License, Version 2.0
+* <http://apache.org/licenses/LICENSE-2.0.html>.
+*/
 
 var Ripples = function(){};
 
